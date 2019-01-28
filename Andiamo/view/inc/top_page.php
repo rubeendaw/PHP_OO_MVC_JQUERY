400b33bbc86992fff957652ac013c14463cc9f95
rubeendaw <35238442+rubeendaw@users.noreply.github.com>
2019-01-28 19:20:18 +0100

    Update top_page.php

--- Andiamo/view/inc/top_page.php
+++ Andiamo/view/inc/top_page.php
@@ -22,6 +22,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA9T9nksTcdLFSZrxyiCnDc0loDBTDheUY&callback=initMap"></script>
+    <script async defer src="https://maps.googleapis.com/maps/api/js?key=KEY"></script>
   </head>
   <body>
